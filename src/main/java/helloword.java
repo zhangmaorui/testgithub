1a4e7e1aba841828a21d08715e67593189e761c6
zhangmaorui <1608915508@qq.com>
2021-04-25 15:03:06 +0800

    更新1

---
+++ src/main/java/helloword.java
@@ -0,0 +1,13 @@
+/**
+ * @author mr
+ * @data 2021/4/25 --- 14:27
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("hello git add 暂存区");
+        System.out.println("hello git add 暂存区");
+        System.out.println("hello git add 暂存区");
+        System.out.println("hello git add 暂存区");
+        System.out.println("hello git add 暂存区");
+    }
+}
